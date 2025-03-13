cb55670ce01118fb936b1d558841264c41e0fdf5
tabatamelisa <tabatavernaza12@gmail.com>
2025-03-13 13:04:46 -0500

    primer commit

---
+++ EdadParaVotar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EdadParaVotar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Validar si el usuario puede votar
+            //Andy Garzón
+            int edad;
+            do
+            {
+                Console.Write("¿Cuál es tu edad? ");
+                edad = int.Parse(Console.ReadLine());
+
+                if (edad >= 18)
+                {
+                    Console.WriteLine("¡Puedes votar!");
+                }
+                else
+                {
+                    Console.WriteLine("No tienes la edad para votar.");
+                }
+
+            } while (edad <= 18); //Repite el programa hasta que se cumpla con la edad 
+        }
+    }
+}
